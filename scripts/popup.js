8f7ee8202c399fc13b1435b890b7b81cd02b9c3a
chandan <b18050@students.iitmandi.ac.in>
2020-04-25 02:05:03 +0530

    hello-world extension

---
+++ scripts/popup.js
@@ -0,0 +1,8 @@
+/*
+// Derived from https://developer.thunderbird.net/add-ons/about-add-ons/hello-world
+// Modified by: Chandan Prakash on 18-04-2020
+// Added modifier's name
+*/
+
+console.log('Hello, World! - from popup.js');
+console.log(' Author - Chandan Prakash')
